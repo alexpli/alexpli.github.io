a4b9b1ad025ae47ac2e1bca1a430207debdfee2e
Alexandre Plisson <alexandre@plisson.xyz>
2017-09-29 23:00:28 +0200

    New version

---
+++ js/main.js
@@ -0,0 +1,2 @@
+(function($){"use strict";$(window).load(function(){$("#loader").fadeOut("slow",function(){$("#preloader").delay(300).fadeOut("slow")})})
+setTimeout(function(){$('#intro h1').fitText(1,{minFontSize:'42px',maxFontSize:'84px'})},100)})(jQuery)
