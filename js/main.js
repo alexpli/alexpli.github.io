89d2de07e7fdec010bd92cf494bfe1c1914548cb
Alexandre P <alexandre@plisson.xyz>
2023-06-06 12:58:19 +0200

    Clean

--- js/main.js
+++
@@ -1,2 +0,0 @@
-(function($){"use strict";$(window).load(function(){$("#loader").fadeOut("slow",function(){$("#preloader").delay(300).fadeOut("slow")})})
-setTimeout(function(){$('#intro h1').fitText(1,{minFontSize:'42px',maxFontSize:'84px'})},100)})(jQuery)
